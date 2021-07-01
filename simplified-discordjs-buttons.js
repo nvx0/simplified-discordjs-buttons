fdece677e0d0201842060a5ffb039e46d9efe206
Jakub Kun <80093182+falseCloud@users.noreply.github.com>
2021-07-01 11:53:39 +0200

    Fixed minor errors.

--- simplified-discordjs-buttons.js
+++ simplified-discordjs-buttons.js
@@ -2,100 +2,101 @@
 // Version r3.5
 
 function create_button(style, url, label, disabled, message) {
-let btn = new disbut.MessageButton()
-
-  // Button style
-
-  if (style === "null" || "NaN" || "undefined" || "none") {
-  btn.setStyle('grey')
-  } else {
-  btn.setStyle(style)
-  }
-
-  // Url 
-
-  if (url === "null" || "NaN" || "undefined" || "none") {
-  // no link
-  } else {
-  btn.setURL(url)
-  }
-
-  // Label
-
-  if (label === "null" || "NaN" || "undefined" || "none") {
-    btn.setLabel('Beep Boop..')
+  let btn = new disbut.MessageButton()
+  
+    // Button style
+  
+    if (style === "null" || "NaN" || "undefined" || "none" || !style) {
+    btn.setStyle('grey')
     } else {
-    btn.setLabel(label)
+    btn.setStyle(style)
     }
-
-    // Disabling
-
-  if (disabled === "true" || "yes") {
-  btn.setDisabled(); 
-  } 
-
-  if (disabled === "false" || "no") {
-    // that button is not disabled, such wow
-  } 
-
-  // Sending message and button, or button without message
-
-  if (message === "null" || "NaN" || "undefined" || "none") {
-    message.channel.send(btn);
-  } else {
-    message.channel.send(message, btn);
-  }
-
-}
-
-function create_button_for_existant_message(style, url, label, disabled, variable) {
- // variable for ex. const myEmbed = = new Discord.MessageEmbed() 
- // qucik example: create_button_for_existant_message("red", "null", "Beop Peob?", "null", "myEmbed")
-let btn = new disbut.MessageButton()
-
-
-  // Button style
-
-  if (style === "null" || "NaN" || "undefined" || "none") {
-  btn.setStyle('grey')
-  } else {
-  btn.setStyle(style)
-  }
-
-  // Url 
-
-  if (url === "null" || "NaN" || "undefined" || "none") {
-  // no link
-  } else {
-  btn.setURL(url)
-  }
-
-  // Label
-
-  if (label === "null" || "NaN" || "undefined" || "none") {
-    btn.setLabel('Beep Boop..')
+  
+    // Url 
+  
+    if (url === "null" || "NaN" || "undefined" || "none" || !url) {
+    // no link
     } else {
-    btn.setLabel(label)
+    btn.setURL(url)
     }
-
-    // Disabling
-
-  if (disabled === "true" || "yes") {
-  btn.setDisabled(); 
-  } 
-
-  if (disabled === "false" || "no") {
-    // that button is not disabled, such wow
-  } 
-
-  // Sending message and button, or button without message
-
-  if (!variable) {
-    console.error("SD.JS Buttons Error: Can't add button to message!")
-  } else {
-    message.channel.send(variable, btn);
+  
+    // Label
+  
+    if (label === "null" || "NaN" || "undefined" || "none" || !label) {
+      btn.setLabel('Beep Boop..')
+      } else {
+      btn.setLabel(label)
+      }
+  
+      // Disabling
+  
+    if (disabled === "true" || "yes") {
+    btn.setDisabled(); 
+    } 
+  
+    if (disabled === "false" || "no") {
+      // that button is not disabled, such wow
+    } 
+  
+    // Sending message and button, or button without message
+  
+    if (message === "null" || "NaN" || "undefined" || "none" || !message) {
+      message.channel.send(btn);
+    } else {
+      message.channel.send(message, btn);
+    }
+  
   }
-
-}
-
-// Easteregg for June: Happy pride month
+  
+  function create_button_for_existant_message(style, url, label, disabled, variable) {
+   // variable for ex. const myEmbed = = new Discord.MessageEmbed() 
+   // qucik example: create_button_for_existant_message("red", "null", "Beop Peob?", "null", "myEmbed")
+  let btn = new disbut.MessageButton()
+  
+  
+    // Button style
+  
+    if (style === "null" || "NaN" || "undefined" || "none" || !style) {
+    btn.setStyle('grey')
+    } else {
+    btn.setStyle(style)
+    }
+  
+    // Url 
+  
+    if (url === "null" || "NaN" || "undefined" || "none" || !url) {
+    // no link
+    } else {
+    btn.setURL(url)
+    }
+  
+    // Label
+  
+    if (label === "null" || "NaN" || "undefined" || "none" || !label) {
+      btn.setLabel('Beep Boop..')
+      } else {
+      btn.setLabel(label)
+      }
+  
+      // Disabling
+  
+    if (disabled === "true" || "yes") {
+    btn.setDisabled(); 
+    } 
+  
+    if (disabled === "false" || "no" || !disabled) {
+      // that button is not disabled, such wow
+    } 
+  
+    // Sending message and button, or button without message
+  
+    if (!variable) {
+      console.error("SD.JS Buttons Error: Can't add button to message!")
+    } else {
+      message.channel.send(variable, btn);
+    }
+  
+  }
+  
+  // Easteregg for June: No easteregg :<
+ 
